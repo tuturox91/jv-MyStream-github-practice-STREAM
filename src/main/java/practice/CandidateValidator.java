689cd6b7d985dcc44ee7da967a095c518886b601
Sniklz <tuturox91@gmail.com>
2025-07-08 18:44:59 +0200

    Finish in 2025

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String SPLITERATOR = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int yearsInUkraine = Arrays.stream(candidate.getPeriodsInUkr()
+                        .split(SPLITERATOR))
+                .mapToInt(Integer::parseInt)
+                .reduce((fromYear, toYear) -> toYear - fromYear).orElseThrow();
+
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && yearsInUkraine >= 10;
+    }
     //write your code here
 }
